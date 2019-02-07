bc13578e8bb2f48efa92b95f4d2a528ead45d620
nuhkoca <nKoca@9234>
2019-02-07 16:18:50 +0300

    completely change observing style in the activity

--- app/src/main/kotlin/io/github/erikcaffrey/livebinding/view/DcCharacterActivity.kt
+++ app/src/main/kotlin/io/github/erikcaffrey/livebinding/view/DcCharacterActivity.kt
@@ -3,44 +3,27 @@ package io.github.erikcaffrey.livebinding.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.GridLayoutManager
+import io.github.erikcaffrey.livebinding.viewmodel.DcCharacterViewModel
 import io.github.erikcaffrey.livedata_databinding.R
 import io.github.erikcaffrey.livedata_databinding.databinding.ActivityDcCharacterBinding
-import io.github.erikcaffrey.livebinding.viewmodel.DcCharacterViewModel
-import kotlinx.android.synthetic.main.activity_dc_character.recycler_dc_character
 
 class DcCharacterActivity : AppCompatActivity() {
-
     private val dcCharacterBinderAdapter: DcCharacterBinderAdapter by lazy { DcCharacterBinderAdapter() }
+    lateinit var activityDcCharacterBinding: ActivityDcCharacterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val activityDcCharacterBinding: ActivityDcCharacterBinding? = DataBindingUtil.setContentView(this, R.layout.activity_dc_character)
-        initRecycler()
-        initObserver(activityDcCharacterBinding)
+        activityDcCharacterBinding = DataBindingUtil.setContentView(this, R.layout.activity_dc_character)
+        initObserver()
     }
 
-    private fun initRecycler() {
-        recycler_dc_character.run {
-            layoutManager = GridLayoutManager(context, 2)
-            setHasFixedSize(true)
-            adapter = dcCharacterBinderAdapter
-        }
-    }
-
-    private fun initObserver(activityDcCharacterBinding: ActivityDcCharacterBinding?) {
+    private fun initObserver() {
         val dcCharacterViewModel = ViewModelProviders.of(this).get(DcCharacterViewModel::class.java)
-
-        dcCharacterViewModel.getDcCharacterList().observe(this, Observer {
-            dcCharacterBinderAdapter.setDcCharacterList(it)
-            dcCharacterBinderAdapter.notifyDataSetChanged()
-        })
-
-        activityDcCharacterBinding?.run {
-            this.dcCharacterViewModel = dcCharacterViewModel
-            setLifecycleOwner(this@DcCharacterActivity)
+        activityDcCharacterBinding.run {
+            recyclerDcCharacter.adapter = dcCharacterBinderAdapter
+            viewmodel = dcCharacterViewModel
+            lifecycleOwner = this@DcCharacterActivity
         }
     }
 }
